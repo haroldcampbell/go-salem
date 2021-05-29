d7465a9fa3f42ff3ce3d8fed68fda679603f5add
haroldcampbell <harold.campbell@gmail.com>
2021-05-29 14:48:42 +0400

    Add example for omitting fields

---
+++ examples/ex-7/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"go-salem"
+	"go_utils/utils"
+)
+
+type sku struct {
+	GUID   string
+	Region string
+}
+
+type basic struct {
+	Tag  *string
+	Cost *float32
+	SKU  sku
+}
+
+// Omitting fields example
+// Use the factory.Omit(...) function to exclude generating values for the specified public fields.
+func main() {
+	factory := salem.Mock(basic{})
+	factory.Omit("Tag"). // Top level field directly in the basic struct
+				Omit("SKU.GUID") // Nested fields
+
+	results := factory.Execute()
+
+	str := utils.PrettyMongoString(results)
+	fmt.Printf("Omitting fields mocks:\n%v\n\n", str)
+}
